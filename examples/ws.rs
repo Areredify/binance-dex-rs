abe7fc8ada2e5f53d063c96c99df1dafb9e2c297
Luke Hamilton <luke@mettadata.com>
2020-07-19 21:53:41 +1000

    Added examples for all ws topics

--- examples/ws.rs
+++ examples/ws.rs
@@ -7,6 +7,10 @@ use std::pin::Pin;
 async fn main() -> Fallible<()> {
     env_logger::init();
 
+    let rune = "RUNE-B1A_BNB".to_string();
+    let ava = "AVA-645_BNB".to_string();
+    let cbm = "CBM-4B2_BNB".to_string();
+
     // good symbol pairs: AVA-645_BNB, CBM-4B2_BNB
     // good order address bnb1w7puzjxu05ktc5zvpnzkndt6tyl720nsutzvpg
     // good transaction address bnb1dn3mhh2gl7vk38w6ppncrvzwzhccvvje49ymkk
@@ -14,10 +18,57 @@ async fn main() -> Fallible<()> {
     let mut websocket = BinanceDexWebsocket::new();
 
     Pin::new(&mut websocket)
-        .subscribe(Topic::Transfers {
-            address: "bnb1dn3mhh2gl7vk38w6ppncrvzwzhccvvje49ymkk".into(),
+        // Transfers
+        // .subscribe(Topic::Transfers {
+        //     address: "bnb1dn3mhh2gl7vk38w6ppncrvzwzhccvvje49ymkk".into(),
+        // })
+        // .await?;
+
+        // Trades
+        .subscribe(Topic::Trades{
+            symbols: vec![rune, ava, cbm],
         })
         .await?;
+
+        // MarketDepth
+        // .subscribe(Topic::MarketDepth{
+        //     amount: None,
+        //     symbols: vec![rune, ava, cbm],
+        // })
+        // .await?;
+
+        // Candlestick
+        // .subscribe(Topic::Candlestick{
+        //     interval: Intervals::T1h,
+        //     symbols: vec![rune, ava, cbm],
+        // })
+        // .await?;
+
+        // Ticker
+        // .subscribe(Topic::Ticker{
+        //     symbols: vec![rune, ava, cbm],
+        // })
+
+        // .await?;
+        // AllTickers
+        // .subscribe(Topic::AllTickers{
+        // })
+        // .await?;
+
+        // MinTicker
+        // .subscribe(Topic::MiniTicker {
+        //     symbols: vec![rune, ava, cbm],
+        // })
+        // .await?;
+
+        // AllMiniTickers
+        // .subscribe(Topic::AllMiniTickers {
+        // })
+        // .await?;
+
+        // Blockheight
+        // .subscribe(Topic::Blockheight)
+        // .await?;
 
     while let Some(msg) = websocket.next().await {
         println!("{:#?}", msg);
